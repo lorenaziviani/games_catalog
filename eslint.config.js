943c85bb2091d9993900839119d2ebc00fe9214c
lorenaziviani <lorena.ziviani.andrade@gmail.com>
2025-07-28 21:40:38 -0300

    chore: init project with Vite + TypeScript + Eslint + Prettier + Husky

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js'
+import prettierConfig from 'eslint-config-prettier'
+import prettier from 'eslint-plugin-prettier'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config([
+  globalIgnores(['dist', 'node_modules', '*.config.js', '*.config.ts']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      prettierConfig
+    ],
+    plugins: {
+      prettier: prettier
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn'
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    }
+  }
+])
